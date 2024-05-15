eac1081102ed9d5425ef8c88c2fbfae861e820ce
tabyretka <134435998+Tabyretkadp@users.noreply.github.com>
2024-05-15 11:56:11 +0000

    Add files via upload

---
+++ TaskList/mainwindow.h
@@ -0,0 +1,42 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QScrollArea>
+#include <QScrollBar>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
+
+private slots:
+    void on_addTask_clicked();
+    void on_showOn_clicked();
+
+    void on_gradeTask_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    int originalWidgetWidth = 0;
+    int originalScrollWidth = 0;
+
+    void loadTasks();
+};
+#endif // MAINWINDOW_H
